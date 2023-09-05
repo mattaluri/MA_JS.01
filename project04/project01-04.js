00944e28c29349a1d7db0fb9fcee61fc573f89ca
Manu venkatesh <mattaluri@uco.edu>
2023-09-05 16:13:20 -0500

    new changes

---
+++ project04/project01-04.js
@@ -0,0 +1,19 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: ManuVenkatesh
+    Date:   04/09/2023
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+var homeStreet = "1 Main St.";
+var homeCity = "Sicilia";
+var homeState = "MA";
+var homeCode = "02103";
+var workStreet = "15 Oak Ln.";
+var workCity = "Central City";
+var workState = "MA";
+var workCode = "02104";
